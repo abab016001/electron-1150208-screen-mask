2a08181cd0a57754d9db0f42b74be6344f27c86c
abab016001 <abab016001@gmail.com>
2026-02-08 12:40:00 +0800

    完成opacity拉霸控制

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, screen, ipcMain } = require('electron');
+
+const path = require('path');
+
+let overlaySettings = {
+  opacity: 0.9
+}
+
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  const win = new BrowserWindow({
+    width: width,
+    height: height,
+    x: 0,
+    y: 0,
+    frame: true, // 移除視窗邊框 false
+    transparent: false, // 開啟視窗透明 true
+    alwaysOnTop: false, // 始終置頂 true
+    fullscreen: false, // 全螢幕 true
+    skipTaskbar: false, // 不在工具列顯示 true
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    show: false
+  })
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+  win.once('ready-to-show', () => {
+    win.show();
+    win.setAlwaysOnTop(true, 'screen-saver');
+    // 當網頁載入完成後，把初始值丟給前端
+    win.webContents.send('init-settings', overlaySettings);
+  });
+
+  ipcMain.on('update-settings', (event, newSettings) => {
+    overlaySettings = { ...overlaySettings, ...newSettings }
+  })
+}
+
+app.whenReady().then(createWindow);
